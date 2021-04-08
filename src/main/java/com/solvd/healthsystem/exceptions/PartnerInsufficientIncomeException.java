ce5be582a9a344708b75d5d3bacd0dc372285bdc
jm-creator <jm-creator@pop-os.localdomain>
2021-04-08 18:14:03 -0300

    reflection project

--- src/main/java/com/solvd/healthsystem/exceptions/PartnerInsufficientIncomeException.java
+++
@@ -1,7 +0,0 @@
-package com.solvd.healthsystem.exceptions;
-
-public class PartnerInsufficientIncomeException extends Exception {
-    public PartnerInsufficientIncomeException() {
-        super("Patient doesn't meet the income restriction");
-    }
-}
---
+++ src/main/java/com/solvd/reflection/Main.java
@@ -0,0 +1,40 @@
+package com.solvd.reflection;
+
+import org.apache.log4j.Logger;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> myObj = Class.forName("java.util.ArrayList");
+
+        LOGGER.info("Get constructors");
+        Arrays.stream(myObj.getConstructors()).forEach(LOGGER::info);
+
+        LOGGER.info("Get methods");
+        Arrays.stream(myObj.getMethods()).forEach(LOGGER::info);
+
+        LOGGER.info("get implemented interfaces");
+        Arrays.stream(myObj.getInterfaces()).forEach(LOGGER::info);
+
+        Constructor<?> constructor = null;
+        try {
+            constructor = myObj.getConstructor();
+        } catch (NoSuchMethodException e) {
+            LOGGER.info(e.getMessage());
+        }
+        Object list = null;
+        try {
+            if (constructor != null) list = constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            LOGGER.info(e.getMessage());
+        }
+        LOGGER.info("ArrayList class: " + list.getClass());
+    }
+
+}
